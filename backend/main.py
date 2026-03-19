ebbb7ca6608c555d8d66c77d5d6a6fd0f2ef4ac0
Erencan Günden <erngndn35@hotmail.com>
2026-03-19 21:32:37 +0100

    Add Vercel URL to CORS

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://checkpoint-delta.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
